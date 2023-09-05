5147537a807bac22fb1afac06aaac6bf845c49c5
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-09-05 20:38:22 -0300

    Add class pessoa e cliente

---
+++ Bet/Modelos/Cliente.cs
@@ -0,0 +1,19 @@
+﻿namespace Bet.Modelos;
+
+internal class Cliente : Pessoa
+{
+    public int Id { get; set; }
+
+    public string Senha { get; set; }
+
+
+    public int GetId()
+    {
+        return Id;
+    }
+
+    public string GetSenha()
+    {
+        return Senha;
+    }
+}
---
+++ Bet/Modelos/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿namespace Bet.Modelos;
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public int Cpf { get; set; }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public int GetIdade()
+        {
+            return Idade;
+        }
+
+        public int GetCpf()
+        {
+            return Cpf;
+        }
+
+
+}
+
