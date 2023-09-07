89d159e2486a5b059e53733bb91c8d534f3ca61b
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-09-07 01:18:07 -0300

    Menu de testes para blackjack

--- Bet/Program.cs
+++ Bet/Program.cs
@@ -5,11 +5,69 @@ using System.Text.Json;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         Baralho baralho = new Baralho();
-        baralho.CriarBaralho();
+        await baralho.CriarBaralho();
+
+        int i = 1;
+
+        while(i != 0)
+        {
+            menu();
+            await Console.Out.WriteAsync("Insira a opc: ");
+            int opcNum = int.Parse(Console.ReadLine());
+
+            switch (opcNum)
+            {
+                case 1:
+                    Console.Clear();
+                    int j = 1;
+                    while(j != 0)
+                    {
+                        await Console.Out.WriteLineAsync("Deseja puxar uma carta : 1- sim 2- nao");
+                        int opcNumCard = int.Parse(Console.ReadLine());
+
+                        if (opcNumCard == 1)
+                        {
+                            await baralho.PuxarCarta();
+
+                        }
+                        else
+                        {
+                            j = 0;
+                           
+                        }
+                    }
+                    
+
+
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Console.Clear();
+                    baralho.mostrarBaralho();
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    i = 0;
+                    break;
+            }
+        }
+        
 
         
+        
+
+        
+    }
+
+    public static void menu()
+    {
+        Console.Clear();
+        Console.WriteLine("-----------------------");
+        Console.WriteLine("1-Iniciar Jogo");
+        Console.WriteLine("2-Mostrar Baralho");
+        Console.WriteLine("-----------------------");
     }
 }
